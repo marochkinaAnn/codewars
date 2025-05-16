27c2dcb3989575ebe56220f6fc00b62a6b83aa2b
Анна <anutochka_15@mail.ru>
2025-05-16 16:32:46 +0300

    solution2

---
+++ 8kyu/Convert-boolean-values.js
@@ -0,0 +1,13 @@
+// ❓Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+//👌 SOLUTION
+function boolToWord( bool ){
+  if(bool){
+    return 'Yes';
+  }else  {
+    return 'No';
+  }
+}
+console.log(boolToWord(0));
+
