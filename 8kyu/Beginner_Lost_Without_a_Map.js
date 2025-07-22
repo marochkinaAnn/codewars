b92cb236cd3244017ce3a0057241c66a1158071f
Анна <anutochka_15@mail.ru>
2025-07-22 14:45:31 +0300

    solution_61

---
+++ 8kyu/Beginner_Lost_Without_a_Map.js
@@ -0,0 +1,11 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//👌 SOLUTION
+function maps(x){
+return x.map(n=>n*2)
+}
+console.log( maps([4, 1, 1, 1, 4]));
