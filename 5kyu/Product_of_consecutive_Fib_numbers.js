21d204a5da2a8fec925ff2fa89caeaa69652da9a
Анна <anutochka_15@mail.ru>
2025-08-06 14:42:01 +0300

    solution_81

---
+++ 5kyu/Product_of_consecutive_Fib_numbers.js
@@ -0,0 +1,55 @@
+// 52290% of 5,86418,205 of 70,485g9644 Issues Reported
+//  JavaScript
+// Node v18.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn): 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+// such that:
+//  F(0)=0  
+// F(1)=1 
+// F(n)=F(n−1)+F(n−2)
+// F(0)=0
+// F(1)=1
+// F(n)=F(n−1)+F(n−2)
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying:
+
+// F(n)∗F(n+1)=prod
+// F(n)∗F(n+1)=prod
+// Your function takes an integer (prod) and returns an array/tuple (check the function signature/sample tests for the return type in your language):
+
+// if F(n) * F(n+1) = prod:
+// (F(n), F(n+1), true)
+// If you do not find two consecutive F(n) verifying F(n) * F(n+1) = prod:
+// (F(n), F(n+1), false)
+// where F(n) is the smallest one such as F(n) * F(n+1) > prod.
+// Examples:
+// 714 ---> (21, 34, true)
+// --> since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+// 800 --->  (34, 55, false)
+// --> since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+
+
+
+//👌 SOLUTION
+function productFib(prod){
+let a = 0
+let b = 1
+while(true){
+    let product = a*b;
+    if (prod===product) {
+        return [a,b,true]
+    } else if (product>prod){
+        return [a,b,false]
+     }else {
+        let next = a+b
+        a = b
+        b = next
+     }
+}
+}
+console.log(productFib(714));
+console.log(productFib(800));
